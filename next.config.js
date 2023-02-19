a173f8d711fc62839c7812879b4a917d3c183af7
Zaid Akhter <zaidakhter1202@gmail.com>
2023-02-19 22:05:59 +0500

    add rewrites

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      {
+        source: "/folder",
+        destination: "/",
+      },
+      {
+        source: "/summary",
+        destination: "/",
+      },
+    ];
+  },
+};
