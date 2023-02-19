dbe363eeb2e3e7bb82217cbd2173e467efced528
Zaid Akhter <zaidakhter1202@gmail.com>
2023-02-19 14:31:07 +0500

    improve button

--- components/Button.js
+++ components/Button.js
@@ -1,17 +1,16 @@
+import classNames from "classnames";
 import React from "react";
 
-export const Button = props => {
-  const { title } = props;
+export const Button = ({ children, className, ...props }) => {
   return (
-    <div>
-      <button
-        type="submit"
-        className="flex-none rounded bg-blue-500 md:py-3 md:px-7  py-2 px-7 font-Monsterrat font-medium leading-7   tracking-wide text-white  hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 disabled:bg-black  
-             disabled:focus:bg-gray-50 "
-        {...props}
-      >
-        {title}
-      </button>
-    </div>
+    <button
+      className={classNames(
+        "flex-none rounded bg-blue-500 py-2 px-7 font-medium leading-7 tracking-wide text-white hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 disabled:bg-black disabled:focus:bg-gray-50 md:py-3 md:px-7",
+        className
+      )}
+      {...props}
+    >
+      {children}
+    </button>
   );
 };
