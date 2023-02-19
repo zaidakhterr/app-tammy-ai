57394c36916863ff119e2037eddd5128df4e37c1
Zaid Akhter <zaidakhter1202@gmail.com>
2023-02-19 14:27:32 +0500

    don't create extra files in pages

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
 import Container from "@/components/Container";
-import Homepage from "./HomePage";
+import { InputBox } from "@/components/InputBox";
+import { Button } from "@/components/Button";
 
 export default function Home() {
   return (
     <Container>
-      {/* <h1 className="text-3xl font-bold underline">
-        Tammy AI</h1> */}
-
-      <Homepage />
+      <div className="my-5  md:flex  md:justify-center">
+        <div className="sm:w-screen  md:w-1/2">
+          <InputBox />
+        </div>
+        <div className=" sm:w-2/2 my-4 md:mx-5 md:my-0   ">
+          <Button title="Summarize" />
+        </div>
+      </div>
     </Container>
   );
 }
