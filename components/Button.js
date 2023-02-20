66c1bb759fb1ddc7003b6d54eb986bc25b9e00ca
Zaid Akhter <zaidakhter1202@gmail.com>
2023-02-20 19:57:56 +0500

    create CreateFolderButton

--- components/Button.js
+++ components/Button.js
@@ -5,7 +5,7 @@ export const Button = ({ children, className, ...props }) => {
   return (
     <button
       className={classNames(
-        "flex h-10 items-center justify-center rounded bg-blue-500 py-2 px-3 text-center text-white transition-colors hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-500 md:h-12 md:px-4",
+        "flex h-10 items-center justify-center rounded bg-blue-500 py-2 px-3 text-center text-white transition-colors hover:bg-blue-600 dark:bg-blue-600 dark:hover:bg-blue-500 md:h-12 md:px-6",
         className
       )}
       {...props}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+import { Fragment, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import { PlusIcon } from "@heroicons/react/24/outline";
 import Container from "@/components/Container";
 import { Button } from "@/components/Button";
 import Input from "@/components/Input";
 import youtubeLogo from "@/assets/youtube.png";
-import { PlusIcon } from "@heroicons/react/24/outline";
+import { Dialog, Transition } from "@headlessui/react";
 
 const CreateSummaryForm = () => {
   const router = useRouter();
@@ -28,17 +30,78 @@ const CreateSummaryForm = () => {
 
 const CreateFolderButton = () => {
   const router = useRouter();
+  const [isOpen, setIsOpen] = useState(false);
 
-  const onClick = () => {
-    console.log("Clicked");
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    closeModal();
+    console.log("Submitted");
     router.push("/folder/123");
   };
 
   return (
-    <Button onClick={onClick} className="ml-auto">
-      <PlusIcon className="mr-2 h-5 w-5 stroke-white" />
-      Create Folder
-    </Button>
+    <>
+      <Button onClick={openModal} className="ml-auto">
+        <PlusIcon className="mr-2 h-5 w-5 stroke-white" />
+        Create Folder
+      </Button>
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={closeModal}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all dark:bg-slate-800">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Create Folder
+                  </Dialog.Title>
+                  <form
+                    onSubmit={onSubmit}
+                    className="mt-4 flex w-full flex-col items-start gap-3"
+                  >
+                    <Input
+                      placeholder="Enter folder name"
+                      containerClassName="w-full"
+                    />
+                    <Button type="submit">Create</Button>
+                  </form>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
   );
 };
 
