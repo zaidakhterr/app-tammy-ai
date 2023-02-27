50b8476c999f2b6e60384897b9cec74b36860a6c
Shaukat Sohail <65353819+naamjee2@users.noreply.github.com>
2023-02-27 14:28:33 +0500

    summary page revamped

--- pages/summary/[id].js
+++ pages/summary/[id].js
@@ -2,7 +2,7 @@ import Container from "@/components/Container";
 
 import { Disclosure } from "@headlessui/react";
 
-import { IconChevronDown, IconShare } from "@tabler/icons-react";
+import { IconChevronDown, IconCopy, IconShare } from "@tabler/icons-react";
 import { useRouter } from "next/router";
 import { useQuery } from "@tanstack/react-query";
 import { fetchSummaryDetailData } from "@/api";
@@ -10,26 +10,25 @@ import { useRef } from "react";
 import YouTube from "react-youtube";
 import { secondsToTime } from "@/utils/index";
 import classNames from "classnames";
+import { US, FR, CH } from "country-flag-icons/react/3x2";
 function Summary({ point, seekTo }) {
   return (
     <>
-      <div className="w-full border-b border-slate-200 dark:border-slate-700 ">
+      <div
+        className={classNames(
+          "w-full border-b border-slate-200 dark:border-slate-700"
+        )}
+      >
         <Disclosure>
           {({ open }) => (
             <>
-              <Disclosure.Button className="md transtion-colors flex w-full justify-between gap-2 px-2 py-4 text-left hover:bg-slate-50 dark:hover:bg-slate-800">
-                <p className="w-full">
+              <Disclosure.Button className="md transtion-colors flex w-full justify-between gap-2 p-4 text-left  text-sm hover:bg-slate-50 dark:hover:bg-slate-800">
+                <p className="w-full ">
                   {point.emoji} {point.description}
                 </p>
                 <span className="flex flex-col items-center gap-3">
-                  <IconChevronDown
-                    className={classNames(
-                      "h-6 w-6 min-w-[1.5rem] transition-transform",
-                      open && "rotate-180"
-                    )}
-                  />
                   <button
-                    className="flex items-center justify-center rounded bg-blue-50 p-1  text-center text-xs text-blue-500 transition-colors hover:bg-blue-100 dark:bg-blue-500/10 dark:text-blue-600 dark:hover:bg-blue-500/20"
+                    className="flex items-center justify-center rounded bg-blue-50 py-0.5 px-1 text-center text-sm text-blue-500 transition-colors hover:bg-blue-100 dark:bg-blue-300/20 dark:text-blue-400 dark:hover:bg-blue-500/20"
                     onClick={e => {
                       e.stopPropagation();
                       seekTo(point.timestamp);
@@ -37,11 +36,17 @@ function Summary({ point, seekTo }) {
                   >
                     {secondsToTime(point.timestamp)}
                   </button>
+                  <IconChevronDown
+                    className={classNames(
+                      "h-6 w-6 min-w-[1.5rem] transition-transform",
+                      open && "rotate-180"
+                    )}
+                  />
                 </span>
               </Disclosure.Button>
 
-              <Disclosure.Panel className="w-full px-2 pt-4 pb-8 text-sm text-slate-600 dark:text-slate-400">
-                <ul className="ml-4 list-disc space-y-2">
+              <Disclosure.Panel className="w-full px-4 pt-4 pb-8  text-slate-500 dark:text-slate-400">
+                <ul className="ml-4 list-disc space-y-2 text-sm">
                   {/* iteration of points array */}
                   {point?.subPoints.map(val => {
                     return <li key={val}>{val}</li>;
@@ -87,9 +92,9 @@ export default function SummaryPage() {
 
   return (
     <>
-      <Container className="summary-grid mt-8 grid gap-8">
+      <Container className="summary-grid grid gap-4 px-0 lg:mt-8 lg:gap-8">
         <YouTube
-          className={"h-full"}
+          className={"sticky top-16 h-full"}
           iframeClassName={"w-full aspect-video sticky top-20 h-auto"}
           frameborder="0"
           allowfullscreen
@@ -100,19 +105,33 @@ export default function SummaryPage() {
           }}
         />
         <div>
-          <div className="flex gap-2 px-2">
-            <button className="flex h-10 items-center justify-center rounded border border-slate-200 bg-transparent bg-right stroke-slate-900 px-2  text-sm transition-colors hover:bg-slate-100 disabled:cursor-not-allowed disabled:bg-slate-100 disabled:stroke-slate-400 dark:border-slate-800 dark:stroke-white dark:hover:bg-slate-800 dark:disabled:bg-slate-800 dark:disabled:stroke-slate-600 ">
+          <div className="flex gap-2 px-4">
+            <button className=" bg-t ransparent flex h-10 items-center justify-center rounded border  border-slate-200 bg-right stroke-slate-900 px-2  text-sm transition-colors hover:bg-slate-100 disabled:cursor-not-allowed disabled:bg-slate-100 disabled:stroke-slate-400 dark:border-slate-800 dark:stroke-white dark:hover:bg-slate-800 dark:disabled:bg-slate-800 dark:disabled:stroke-slate-600 ">
+              <IconCopy className="h-5 w-5 stroke-1" />
+            </button>
+            <button className=" bg-t ransparent flex h-10 items-center justify-center rounded border  border-slate-200 bg-right stroke-slate-900 px-2  text-sm transition-colors hover:bg-slate-100 disabled:cursor-not-allowed disabled:bg-slate-100 disabled:stroke-slate-400 dark:border-slate-800 dark:stroke-white dark:hover:bg-slate-800 dark:disabled:bg-slate-800 dark:disabled:stroke-slate-600 ">
               <IconShare className="h-5 w-5 stroke-1" />
             </button>
-            <select className="flex h-10 items-center justify-center rounded border border-slate-200 bg-transparent stroke-slate-900 px-2 pr-8 text-sm transition-colors hover:bg-slate-100 disabled:cursor-not-allowed disabled:bg-slate-100 disabled:stroke-slate-400 dark:border-slate-800 dark:stroke-white dark:hover:bg-slate-800 dark:disabled:bg-slate-800 dark:disabled:stroke-slate-600 ">
-              {["English", "French", "Chinese"].map(language => (
-                <option key={language} value={language}>
-                  {language}
+            <select className="flex h-10 cursor-pointer  items-center justify-center rounded border border-slate-200 bg-transparent stroke-slate-900 px-2 pr-8 text-sm transition-colors hover:bg-slate-100 disabled:cursor-not-allowed disabled:bg-slate-100 disabled:stroke-slate-400 dark:border-slate-800 dark:stroke-white dark:hover:bg-slate-800 dark:disabled:bg-slate-800 dark:disabled:stroke-slate-600">
+              {[
+                {
+                  name: "English",
+                  icon: <US className="h-5" />,
+                },
+                { name: "French", icon: <FR /> },
+                { name: "Chinese", icon: <CH /> },
+              ].map((language, index) => (
+                <option
+                  value={language}
+                  key={index}
+                  className="px-6 dark:bg-slate-800 "
+                >
+                  {language?.name}
                 </option>
               ))}
             </select>
           </div>
-          <p className="mt-2 p-2 font-bold">{data.description}</p>
+          <p className="mt-2 py-2 px-4 text-sm font-bold">{data.description}</p>
           {points.map(point => {
             return <Summary key={point.id} point={point} seekTo={seekTo} />;
           })}
