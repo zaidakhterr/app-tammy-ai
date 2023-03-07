c64825f2b98f87132057a68a99dcdd8bf46c97f2
Zaid Akhter <zaidakhter1202@gmail.com>
2023-03-07 18:20:18 +0500

    fix logo

--- components/Logo.js
+++ components/Logo.js
@@ -4,8 +4,8 @@ const Logo = () => {
   return (
     <p className="flex items-center text-xl font-black text-blue-600 dark:text-white md:text-2xl">
       <IconCircleLetterT className="mr-1 h-7 w-7 stroke-blue-600 dark:stroke-white" />
-      Tammy
-      <span className="font-normal text-blue-600 dark:text-white">.Ai</span>
+      tammy
+      <span className="font-normal text-blue-600 dark:text-white">.ai</span>
     </p>
   );
 };
